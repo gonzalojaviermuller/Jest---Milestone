a15ab701d0a34d4b8caf7131fadec388b4f8441e
Gonzalo Müller <gonzalojaviermuller@gmail.com>
2021-12-17 15:56:48 -0300

    Deleted Console.log
    -Deleted all Console.log
    -Eslintr.json moved into package.josn
    -Added Optional chaining when accessing properties of objects

--- petStore.spec.js
+++ petStore.spec.js
@@ -10,29 +10,20 @@ describe("Post /pet, Get /Pet/id", () => {
         id: 2,
         name: "Shark",
       });
-
-    console.log(response.headers);
-    console.log(response.body);
-
     expect(response.statusCode).toBe(200);
-    expect(response.body.name).toEqual(expect.stringContaining("Shark"));
+    expect(response.body?.name).toEqual(expect.stringContaining("Shark"));
     expect(response.headers["content-type"]).toEqual(
       expect.stringContaining("json")
     );
 
-    petID = response.body.id;
-
-    console.log(petID);
+    petID = response.body?.id;
   });
 
   test("Should Get posted pet", async () => {
     const response = await request("https://petstore.swagger.io/v2").get(
       `/pet/${petID}`
     );
-
-    console.log(response.body.id);
-
     expect(response.statusCode).toBe(200);
-    expect(response.body.id).toEqual(petID);
+    expect(response.body?.id).toEqual(petID);
   });
 });
