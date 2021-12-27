1a36b1aedf07f85c9052fac7e2994c8f9b1e1140
Gonzalo Müller <86006789+gonzalojaviermuller@users.noreply.github.com>
2021-12-27 14:32:17 -0300

    Refactor/create modules (#3)
    * Saving before adding Babel Plugin
    
    * Added modules for getPet and postPet
    
    -Created modules for getPet and postPet
    -Added default params for modules functions
    -Installed Babel pluggin for importing modules
    -Added name assertion to Get test
    
    * Added unique ID
    
    -Unique ID for tests
    -Changed support/petObject function

--- petStore.spec.js
+++ petStore.spec.js
@@ -1,29 +1,28 @@
-const request = require("supertest");
+import postPet from "./modules/postPet";
+import getPet from "./modules/getPet";
 
 describe("Post /pet, Get /Pet/id", () => {
   let petID;
+  let petName;
 
   test("Should Post new pet", async () => {
-    const response = await request("https://petstore.swagger.io/v2")
-      .post("/pet")
-      .send({
-        id: 2,
-        name: "Shark",
-      });
-    expect(response.statusCode).toBe(200);
-    expect(response.body?.name).toEqual(expect.stringContaining("Shark"));
-    expect(response.headers["content-type"]).toEqual(
-      expect.stringContaining("json")
+    const id = Date.now();
+    const postPetRes = await postPet(200, {id: id, name: "Sharky"});
+
+    expect(postPetRes.response?.body?.name).toEqual("Sharky");
+    expect(postPetRes.response?.headers["content-type"]).toEqual(
+      "application/json"
     );
 
-    petID = response.body?.id;
+    petID = postPetRes.response?.body?.id;
+    petName = postPetRes.response?.body?.name;
   });
 
   test("Should Get posted pet", async () => {
-    const response = await request("https://petstore.swagger.io/v2").get(
-      `/pet/${petID}`
-    );
-    expect(response.statusCode).toBe(200);
-    expect(response.body?.id).toEqual(petID);
+    const getPetRes = await getPet(200, {id:petID});
+
+    console.log(getPetRes.response.body.id)
+    expect(getPetRes.response?.body?.id).toEqual(petID);
+    expect(getPetRes.response?.body?.name).toEqual(petName);
   });
 });
